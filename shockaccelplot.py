7246cf1631e1842adcc039b3e052b1d0498de70f
Joshua Tenorio <joshliam09@gmail.com>
2024-04-21 22:37:12 -0700

    shock velocity graphs

--- shockaccelplot.py
+++ shockaccelplot.py
@@ -1,28 +1,118 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def meow(path, day, event, runno):
+def latplot(path, event, runno):
     df = pd.read_csv(path)
     print(path)
 
     fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nshaft velocity vs lateral acceleration\n2 turns highspeed compression")
+    
+    ax.set_xlabel("lateral acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
 
-    plt.suptitle("File: " + path + "\nright shocks and longitudinal acceleration\n240419")
+    flt_df = df.loc[(df["longitudinal accel (mG)"] <= 250) & (df["longitudinal accel (mG)"] >= -250)]
+
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["rl shock velocity (mm/s)"], s=1, label="RL", alpha=0.5)
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["rr shock velocity (mm/s)"], s=1, label="RR", alpha=0.5)
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["fr shock velocity (mm/s)"], s=1, label="FR", alpha=0.5)
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["fl shock velocity (mm/s)"], s=1, label="FL", alpha=0.5)
+    ax.legend()
+    plt.savefig("reports/240419/vel/" + runno + "-shocks-lateral-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nFL shaft velocity vs lateral acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("lateral acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["fl shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-FL-shocks-lateral-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nFR shaft velocity vs lateral acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("lateral acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["fr shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-FR-shocks-lateral-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nRR shaft velocity vs lateral acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("lateral acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["rr shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-RR-shocks-lateral-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nRL shaft velocity vs lateral acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("lateral acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["lateral accel (mG)"], flt_df["rl shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-RL-shocks-lateral-" + event + ".png")
+
+def longplot(path, event, runno):
+    df = pd.read_csv(path)
+    print(path)
+
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nshaft velocity vs longitudinal acceleration\n2 turns highspeed compression")
+    
     ax.set_xlabel("longitudinal acceleration (mG)")
-    ax.set_ylabel("shock displacement (mm)")
+    ax.set_ylabel("shock velocity (mm/s)")
 
-    # "fr shock velocity (mm/s)"
-    # "fr displacement (mm)"
-    plt.scatter(df["longitudinal accel (mG)"], df["fr displacement (mm)"], s=2, c='r', label="FR")
-    plt.scatter(df["longitudinal accel (mG)"], df["rr displacement (mm)"], s=2, c='b', label="RR")
-    plt.legend()
+    flt_df = df.loc[(df["lateral accel (mG)"] <= 250) & (df["lateral accel (mG)"] >= -250)]
 
-    plt.savefig("reports/240419/rightshocks-" + event + runno + ".png")
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["rl shock velocity (mm/s)"], s=1, label="RL", alpha=0.5)
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["rr shock velocity (mm/s)"], s=1, label="RR", alpha=0.5)
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["fr shock velocity (mm/s)"], s=1, label="FR", alpha=0.5)
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["fl shock velocity (mm/s)"], s=1, label="FL", alpha=0.5)
+    ax.legend()
+    plt.savefig("reports/240419/vel/" + runno + "-shocks-longitudinal-" + event + ".png")
 
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nFL shaft velocity vs longitudinal acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("longitudinal acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["fl shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-FL-shocks-longitudinal-" + event + ".png")
 
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nFR shaft velocity vs longitudinal acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("longitudinal acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["fr shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-FR-shocks-longitudinal-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nRR shaft velocity vs longitudinal acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("longitudinal acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["rr shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-RR-shocks-longitudinal-" + event + ".png")
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    fig.suptitle("File: " + path + "; 240419\nRL shaft velocity vs longitudinal acceleration\n2 turns highspeed compression")
+    ax.set_xlabel("longitudinal acceleration (mG)")
+    ax.set_ylabel("shock velocity (mm/s)")
+    ax.scatter(flt_df["longitudinal accel (mG)"], flt_df["rl shock velocity (mm/s)"], s=1)
+    plt.savefig("reports/240419/vel/" + runno + "-RL-shocks-longitudinal-" + event + ".png")
+
+# accel
+longplot("processed/everything/data69.csv", "accel", "69")
+latplot("processed/everything/data69.csv", "accel", "69")
+
+# shakedown
 for i in range(64, 67):
-    meow("processed/everything/data" + str(i) + ".csv", "240419", "shakedown", str(i))
+    longplot("processed/everything/data" + str(i) + ".csv", "shakedown", str(i))
+    latplot("processed/everything/data" + str(i) + ".csv", "shakedown", str(i))
 
-meow("processed/everything/data69.csv", "240419", "accel", str(69))
-meow("processed/everything/data72.csv", "240419", "aero-coastdown", str(72))
-meow("processed/everything/data73.csv", "240419", "aero-coastdown", str(73))
+# coastdown aero
+for i in range(72, 74):
+    longplot("processed/everything/data" + str(i) + ".csv", "coastdown-aero", str(i))
+    latplot("processed/everything/data" + str(i) + ".csv", "coastdown-aero", str(i))
