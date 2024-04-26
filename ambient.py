2d502dde002b587a560baddf692ed3df2a5c2966
Joshua Tenorio <joshliam09@gmail.com>
2024-04-26 09:33:54 -0700

    ambient plots

--- ambient.py
+++ ambient.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("processed/everything/data66.csv")
+#df = pd.read_csv("processed/240407/evening/data199.csv")
+df = pd.read_csv("processed/coastdown-240418/data48.csv")
 
 print(df["fl wheel ambient temp (C)"].describe())
 print(df["fr wheel ambient temp (C)"].describe())
 print(df["fr rotor temp (C)"].describe())
+
+plt.plot(df["time (s)"], df["fl wheel ambient temp (C)"], c='r')
+plt.plot(df["time (s)"], df["fl rotor temp (C)"], c='b')
+
+plt.plot(df["time (s)"], df["fr wheel ambient temp (C)"], c='g')
+plt.plot(df["time (s)"], df["fr rotor temp (C)"], c='orange')
+plt.show()
